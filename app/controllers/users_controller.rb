d9ed81827b29b3d042233ee46007f3359a54c9d3
Ryan Chang <ry4nch4ng@gmail.com>
2016-04-12 15:19:12 -0700

    user#show now shows according to name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: :show
+  before_action :admin_user,     only: :index
 
 include EntriesHelper
+
+  def index
+  end
 
   def show
     @user = User.find(params[:id])
     @entries = @user.entries
     split_entries(@entries)
     @entry = current_user.entries.build if logged_in?
+
+    if @user == current_user
+      @title1 = "Your Queue"
+      @title2 = "Your Archive"
+    else
+      @title1 = "#{@user.name}'s Queue"
+      @title2 = "#{@user.name}'s Archive"
+    end
   end
 
   def new
