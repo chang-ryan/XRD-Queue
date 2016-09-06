d64e36db6e4dd28bb2112e2f223dbc047c32cc8c
Ryan Chang <ry4nch4ng@gmail.com>
2016-09-06 11:59:55 -0700

    add appt spec

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  it "has a valid factory" do
-    Factory.create(:appointment).should be_valid
+  before do
+    @ryan = User.create(name: 'Ryan', email: 'rchang@hrl.com', password: 'asdf', password_confirmation: 'asdf')
+    @start_time = Time.now
+    @end_time   = @start_time + 2000
+    @appt = @ryan.appointments.create(start_time: @start_time, end_time: @end_time)
   end
 
-  it 'is invalid without start time'
-  it 'is invalid without end time'
-  it 'is invalid without a corresponding user id'
-  it 'is invalid when overlapping'
+  it 'is invalid without start time' do
+    expect(Appointment.new(end_time: Time.now)).to_not be_valid
+  end
+
+  it 'is invalid without end time' do
+    expect(Appointment.new(start_time: Time.now)).to_not be_valid
+  end
+
+  it 'is invalid without a corresponding user id' do
+    expect(Appointment.new(start_time: Time.now, end_time: Time.now+2000)).to_not be_valid
+  end
+
+  context 'overlapping points' do
+    it 'is invalid when it ends inside another' do
+      expect(Appointment.new(start_time: @start_time-2000, end_time: @end_time-1999)).to_not be_valid
+    end
+    it 'is invalid when it starts inside another' do
+      expect(Appointment.new(start_time: @start_time+1999, end_time: @end_time+2000)).to_not be_valid
+    end
+    it 'is invalid when it is entirely in another' do
+      expect(Appointment.new(start_time: @start_time+1, end_time: @end_time-1)).to_not be_valid
+    end
+    it 'is invalid when it is entirely around another' do
+      expect(Appointment.new(start_time: @start_time-1, end_time: @end_time+1)).to_not be_valid
+    end
+  end
+
+  it 'is valid with all correct fields' do
+    expect(@ryan.appointments.new(start_time: @end_time+1, end_time: @end_time+2000)).to be_valid
+  end
 end
